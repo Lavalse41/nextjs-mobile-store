4c39e49ac9fce792d9a0a1d9463fe79a85af64b6
Ratiporn <ratiporn.pr@gmail.com>
2023-11-21 16:45:26 +0700

    style: home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,24 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      background: "#f9fafa",
+      gray01: "#f7f7f8",
+      gray02: "#bac3c3",
+      gray03: "#97a1aa",
+      gray04: "#535c65",
+      main: "#00cdac",
+      accent01: "#e3f6f4",
+      accent02: "#00a79d",
+    },
     extend: {
       backgroundImage: {},
     },
+    fontFamily: {
+      prompt: ["Prompt"],
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
+
 export default config;
